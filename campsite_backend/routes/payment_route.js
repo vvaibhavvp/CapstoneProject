b1a31a89bb827130bdccd48722a14f8cc932c4c5
Vaibhav Parte <vaibhavparte390@gmail.com>
2025-04-17 16:40:45 -0400

    Delete campsite_backend directory

--- campsite_backend/routes/payment_route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { processPayment, getBookingDetails } from "../controller/payment_controller.js";
-
-const router = express.Router();
-router.post("/", processPayment);
-
-router.get("/:bookingId", getBookingDetails);
-
-export default router;
